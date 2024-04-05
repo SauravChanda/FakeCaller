ff10a39e9c640995d159466e2a4be0d3f17a08f6
SauravChanda <sauravchanda9@gmail.com>
2024-04-05 21:00:39 +0530

    fix: 🐛 Cannot mark ourself as spam

--- src/controllers/spam.controller.ts
+++ src/controllers/spam.controller.ts
@@ -2,23 +2,25 @@ import httpStatus from "http-status";
 import catchAsync from "../utils/catchAsync";
 import { spamService } from "../services";
 import { User } from "@prisma/client";
+import ApiError from "../utils/ApiError";
 
 const markSpam = catchAsync(async (req, res) => {
   const { phoneNumber } = req.body;
   const user = req.user as User;
+  if (user.phoneNumber === phoneNumber)
+    throw new ApiError(httpStatus.BAD_REQUEST, "Cannot mark ourselves as spam");
   const spamEntry = await spamService.markSpam(phoneNumber, user.id);
   res.status(httpStatus.CREATED).send("Marked Spam!");
 });
 
 const unmarkSpam = catchAsync(async (req, res) => {
-    const { phoneNumber } = req.body;
-    const user = req.user as User;
-    const spamEntry = await spamService.unmarkSpam(phoneNumber, user.id);
-    res.status(httpStatus.CREATED).send("Removed Spam!");
-  });
-  
+  const { phoneNumber } = req.body;
+  const user = req.user as User;
+  const spamEntry = await spamService.unmarkSpam(phoneNumber, user.id);
+  res.status(httpStatus.CREATED).send("Removed Spam!");
+});
 
 export default {
   markSpam,
-  unmarkSpam
+  unmarkSpam,
 };
